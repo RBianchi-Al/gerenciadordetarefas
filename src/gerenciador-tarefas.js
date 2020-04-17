bafdb509d3a0f25784ac3320f5c574a3e9a5b29e
RBianchi-Al <renatalebianchi@gmail.com>
2020-04-17 19:05:54 -0300

    first commit

---
+++ src/gerenciador-tarefas.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './gerenciador-tarefas.css';
+
+function GerenciadorTarefas() {
+  return (
+    <div >
+      <h1>Gerenciador Tarefas</h1>
+      
+    </div>
+  );
+}
+
+export default GerenciadorTarefas;
